548568650cfe54a8655c8e3c9fae2f45e47e9df1
trenchgun <sirpyrotiger@gmail.com>
2021-04-30 23:24:57 -0500

    added script_macros for future use

---
+++ addons/main/script_macros.hpp
@@ -0,0 +1,12 @@
+// sides
+#define BLUFOR 0
+#define OPFOR 1
+#define INDFOR 2	
+#define CIVILIAN 3
+
+// textures	--> currently does not seem to work 
+//				possible problem with behavior of defines from different locations
+#define TEX(COMPONENT,NAME,VARIANT) "\tmtm\pack\##COMPONENT##\Data\tmtm_##NAME##_##VARIANT##_co.paa"
+
+
+
