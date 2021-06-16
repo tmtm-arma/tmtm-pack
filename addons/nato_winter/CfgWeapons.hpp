f485a90f15ef880e713bdffb95e9f56377d91071
Superxpdude <superxpdude@gmail.com>
2021-06-16 11:22:22 -0500

    Added initial NATO winter textures

---
+++ addons/nato_winter/CfgWeapons.hpp
@@ -0,0 +1,52 @@
+class CfgWeapons {
+	// Helmets
+	class H_HelmetB;
+	class tmtm_h_helmetb_white: H_HelmetB {
+		author = "Superxpdude";
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "Combat Helmet (White)";
+		picture="\tmtm\pack\nato_winter\data\ui\icon_tmtm_combat_helmet_white.paa";
+		hiddenSelectionsTextures[] = {"\tmtm\pack\nato_winter\data\tmtm_combat_helmet_white.paa"};
+	};
+	
+	class H_HelmetSpecB;
+	class tmtm_h_helmetspecb_white: H_HelmetSpecB {
+		author = "Superxpdude";
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "Enhanced Combat Helmet (White)";
+		picture="\tmtm\pack\nato_winter\data\ui\icon_tmtm_combat_helmet_spec_white.paa";
+		hiddenSelectionsTextures[] = {"\tmtm\pack\nato_winter\data\tmtm_combat_helmet_white.paa"};
+	};
+	
+	class H_HelmetB_light;
+	class tmtm_h_helmetb_light_white: H_HelmetB_light {
+		author = "Superxpdude";
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "Light Combat Helmet (White)";
+		picture="\tmtm\pack\nato_winter\data\ui\icon_tmtm_combat_helmet_light_white.paa";
+		hiddenSelectionsTextures[] = {"\tmtm\pack\nato_winter\data\tmtm_combat_helmet_white.paa"};
+	};
+	
+	
+	// Vests
+	class V_PlateCarrier1_blk;
+	class tmtm_v_platecarrier1_darkgreen: V_PlateCarrier1_blk {
+		author = "Superxpdude";
+		scope = 2;
+		picture = "\tmtm\pack\nato_winter\data\ui\icon_tmtm_v_plate_carrier_darkgreen_lite.paa";
+		displayName = "Carrier Lite (Dark Green)";
+		hiddenSelectionsTextures[] = {"\tmtm\pack\nato_winter\data\tmtm_plate_carrier_darkgreen.paa"};
+	};
+	
+	class V_PlateCarrier2_blk;
+	class tmtm_v_platecarrier2_darkgreen: V_PlateCarrier2_blk {
+		author = "Superxpdude";
+		scope = 2;
+		picture = "\tmtm\pack\nato_winter\data\ui\icon_tmtm_v_plate_carrier_darkgreen.paa";
+		displayName = "Carrier Rig (Dark Green)";
+		hiddenSelectionsTextures[] = {"\tmtm\pack\nato_winter\data\tmtm_plate_carrier_darkgreen.paa"};
+	};
+};
