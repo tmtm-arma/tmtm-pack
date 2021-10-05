44c94c51b8fab1637ef6b79a78ef8da3baaea7f3
trenchgun <sirpyrotiger@gmail.com>
2021-10-05 01:06:45 -0500

    added ACH/MICH 2000 retextures

---
+++ addons/cup_headgear/CfgWeapons.hpp
@@ -0,0 +1,406 @@
+class CfgWeapons
+{
+	class ItemCore;
+	class HeadgearItem;
+	class HelmetBase: ItemCore
+	{
+		class ItemInfo;
+	};
+	
+	
+	// Basic ACH helmet
+	class CUP_H_USArmy_HelmetACH_UCP: ItemCore {};
+	class tmtm_h_helmetACH_black: CUP_H_USArmy_HelmetACH_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (Black)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_black_co.paa"
+		};
+	};
+	class tmtm_h_helmetACH_atacsAu: CUP_H_USArmy_HelmetACH_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS AU)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsAu_co.paa"
+		};
+	};
+	class tmtm_h_helmetACH_atacsFg: CUP_H_USArmy_HelmetACH_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS FG)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsFg_co.paa"
+		};
+	};
+	class tmtm_h_helmetACH_emr: CUP_H_USArmy_HelmetACH_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emr_co.paa"
+		};
+	};
+	class tmtm_h_helmetACH_emrWinter: CUP_H_USArmy_HelmetACH_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR Winter)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emrWinter_co.paa"
+		};
+	};
+	class tmtm_h_helmetACH_surpat: CUP_H_USArmy_HelmetACH_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (SURPAT)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_surpat_co.paa"
+		};
+	};
+	class tmtm_h_helmetACH_white: CUP_H_USArmy_HelmetACH_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (White)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_whiteGrey_co.paa"
+		};
+	};
+	
+	// ACH helmet + headset
+	class CUP_H_USArmy_HelmetACH_Headset_UCP: ItemCore {};
+	class tmtm_h_helmetACHHeadset_black: CUP_H_USArmy_HelmetACH_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (Black/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_black_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHHeadset_atacsAu: CUP_H_USArmy_HelmetACH_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS AU/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsAu_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHHeadset_atacsFg: CUP_H_USArmy_HelmetACH_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS FG/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsFg_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHHeadset_emr: CUP_H_USArmy_HelmetACH_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emr_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHHeadset_emrWinter: CUP_H_USArmy_HelmetACH_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR Winter/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emrWinter_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHHeadset_surpat: CUP_H_USArmy_HelmetACH_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (SURPAT/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_surpat_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHHeadset_white: CUP_H_USArmy_HelmetACH_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (White/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_whiteGrey_co.paa"
+		};
+	};
+	
+	// ACH Helmet + Goggles
+	class CUP_H_USArmy_HelmetACH_ESS_UCP: ItemCore {};
+	class tmtm_h_helmetACHGoggles_black: CUP_H_USArmy_HelmetACH_ESS_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (Black/Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_black_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGoggles_atacsAu: CUP_H_USArmy_HelmetACH_ESS_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS AU/Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsAu_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGoggles_atacsFg: CUP_H_USArmy_HelmetACH_ESS_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS FG/Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsFg_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGoggles_emr: CUP_H_USArmy_HelmetACH_ESS_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR/Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emr_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGoggles_emrWinter: CUP_H_USArmy_HelmetACH_ESS_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR Winter/Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emrWinter_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGoggles_surpat: CUP_H_USArmy_HelmetACH_ESS_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (SURPAT/Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_surpat_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGoggles_white: CUP_H_USArmy_HelmetACH_ESS_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (White/Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_whiteGrey_co.paa"
+		};
+	};
+	
+	// ACH Helmet + Headset + Goggles
+	class CUP_H_USArmy_HelmetACH_ESS_Headset_UCP: ItemCore {};
+	class tmtm_h_helmetACHGogglesHeadset_black: CUP_H_USArmy_HelmetACH_ESS_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (Black/Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_black_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGogglesHeadset_atacsAu: CUP_H_USArmy_HelmetACH_ESS_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS AU/Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsAu_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGogglesHeadset_atacsFg: CUP_H_USArmy_HelmetACH_ESS_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS FG/Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsFg_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGogglesHeadset_emr: CUP_H_USArmy_HelmetACH_ESS_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR/Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emr_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGogglesHeadset_emrWinter: CUP_H_USArmy_HelmetACH_ESS_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR Winter/Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emrWinter_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGogglesHeadset_surpat: CUP_H_USArmy_HelmetACH_ESS_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (SURPAT/Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_surpat_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHGogglesHeadset_white: CUP_H_USArmy_HelmetACH_ESS_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (White/Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_whiteGrey_co.paa"
+		};
+	};
+	
+	// ACH Helmet + Covered Goggles
+	class CUP_H_USArmy_HelmetACH_GCOVERED_UCP: ItemCore {};
+	class tmtm_h_helmetACHCoveredGoggles_black: CUP_H_USArmy_HelmetACH_GCOVERED_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (Black/Covered Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_black_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGoggles_atacsAu: CUP_H_USArmy_HelmetACH_GCOVERED_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS AU/Covered Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsAu_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGoggles_atacsFg: CUP_H_USArmy_HelmetACH_GCOVERED_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS FG/Covered Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsFg_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGoggles_emr: CUP_H_USArmy_HelmetACH_GCOVERED_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR/Covered Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emr_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGoggles_emrWinter: CUP_H_USArmy_HelmetACH_GCOVERED_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR Winter/Covered Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emrWinter_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGoggles_surpat: CUP_H_USArmy_HelmetACH_GCOVERED_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (SURPAT/Covered Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_surpat_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGoggles_white: CUP_H_USArmy_HelmetACH_GCOVERED_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (White/Covered Goggles)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_whiteGrey_co.paa"
+		};
+	};
+	
+	// ACH Helmet + Covered Goggles + Helmet
+	class CUP_H_USArmy_HelmetACH_GCOVERED_Headset_UCP: ItemCore {};
+	class tmtm_h_helmetACHCoveredGogglesHeadset_black: CUP_H_USArmy_HelmetACH_GCOVERED_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (Black/Covered Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_black_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGogglesHeadset_atacsAu: CUP_H_USArmy_HelmetACH_GCOVERED_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS AU/Covered Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsAu_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGogglesHeadset_atacsFg: CUP_H_USArmy_HelmetACH_GCOVERED_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (ATACS FG/Covered Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_atacsFg_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGogglesHeadset_emr: CUP_H_USArmy_HelmetACH_GCOVERED_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR/Covered Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emr_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGogglesHeadset_emrWinter: CUP_H_USArmy_HelmetACH_GCOVERED_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (EMR Winter/Covered Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_emrWinter_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGogglesHeadset_surpat: CUP_H_USArmy_HelmetACH_GCOVERED_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (SURPAT/Covered Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_surpat_co.paa"
+		};
+	};
+	class tmtm_h_helmetACHCoveredGogglesHeadset_white: CUP_H_USArmy_HelmetACH_GCOVERED_Headset_UCP
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="MICH 2000 (White/Covered Goggles/Headset)";
+		hiddenSelectionsTextures[]={
+			"\tmtm\pack\cup_headgear\Data\tmtm_mich2000_whiteGrey_co.paa"
+		};
+	};
+};
---
+++ addons/cup_headgear/README.md
@@ -0,0 +1,10 @@
+# CUP Headgear
+Retextures of Community Upgrade Project headgear.
+
+## ACH / MICH 2000 Helmet
+	- Black
+	- White
+	- ATACS AU
+	- ATACS FG
+	- EMR
+	- SURPAT
