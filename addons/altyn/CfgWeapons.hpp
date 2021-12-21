80a151715bc827c34437a0b71eac3b12a3d77412
trenchgun <sirpyrotiger@gmail.com>
2021-12-20 18:22:49 -0600

    adjusted config for new helmets, removed unused textures

--- addons/altyn/CfgWeapons.hpp
+++ addons/altyn/CfgWeapons.hpp
@@ -2,11 +2,6 @@ class CfgWeapons
 {
 	class ItemCore;
 	class HeadgearItem;
-	class HelmetBase: ItemCore
-	{
-		class ItemInfo;
-	};
-	class H_HelmetB;
 	
 	class tmtm_h_altyn_green: ItemCore
 	{
@@ -15,28 +10,27 @@ class CfgWeapons
 		displayName="Altyn (Green)";
 		descriptionShort = "Armor Level IV";
 		picture="\A3\characters_f\Data\UI\icon_H_helmet_plain_ca.paa";
-		model="\tmtm\pack\altyn\tmtm_altyn.p3d";
+		model="\tmtm\pack\altyn\tmtm_altyn_noVisor.p3d";
 		hiddenSelections[]={"camo"};
-		hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_green_ca.paa"};
+		hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_green_co.paa"};
 		class ItemInfo: HeadgearItem
 		{
 			mass=45;
-			uniformModel="\tmtm\pack\altyn\tmtm_altyn.p3d";
+			uniformModel="\tmtm\pack\altyn\tmtm_altyn_noVisor.p3d";
 			modelSides[]={6};
 			hiddenSelections[]={"camo"};
-			hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_green_ca.paa"};
+			hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_green_co.paa"};
 			class HitpointsProtectionInfo
 			{
 				class Head
 				{
 					hitpointName="HitHead";
 					armor=10;
-					passThrough=0.5;
+					passThrough=0.1;
 				};
 			};
 		};
 	};
-	
 	class tmtm_h_altyn_olive: tmtm_h_altyn_green
 	{
 		scope=2;
@@ -57,5 +51,106 @@ class CfgWeapons
 			hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_black_co.paa"};
 		};
 	};
-	
+	class tmtm_h_altynVisorUp_green: ItemCore
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="Altyn (Green/Visor Up)";
+		descriptionShort = "Armor Level IV";
+		picture="\A3\characters_f\Data\UI\icon_H_helmet_plain_ca.paa";
+		model="\tmtm\pack\altyn\tmtm_altyn.p3d";
+		hiddenSelections[]={"camo"};
+		hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_green_co.paa"};
+		class ItemInfo: HeadgearItem
+		{
+			mass=45;
+			uniformModel="\tmtm\pack\altyn\tmtm_altyn.p3d";
+			modelSides[]={6};
+			hiddenSelections[]={"camo"};
+			hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_green_co.paa"};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitpointName="HitHead";
+					armor=10;
+					passThrough=0.1;
+				};
+			};
+		};
+	};
+	class tmtm_h_altynVisorUp_olive: tmtm_h_altynVisorUp_green
+	{
+		scope=2;
+		displayName="Altyn (Olive/Visor Up)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_olive_co.paa"};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_olive_co.paa"};
+		};
+	};
+	class tmtm_h_altynVisorUp_black: tmtm_h_altynVisorUp_green
+	{
+		scope=2;
+		displayName="Altyn (Black/Visor Up)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_black_co.paa"};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_black_co.paa"};
+		};
+	};
+	class tmtm_h_altynVisorDown_green: ItemCore
+	{
+		author="Trenchgun";
+		scope=2;
+		displayName="Altyn (Green/Visor Down)";
+		descriptionShort = "Armor Level IV";
+		picture="\A3\characters_f\Data\UI\icon_H_helmet_plain_ca.paa";
+		model="\tmtm\pack\altyn\tmtm_altyn_visorDown.p3d";
+		hiddenSelections[]={"camo"};
+		hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_green_co.paa"};
+		class ItemInfo: HeadgearItem
+		{
+			mass=45;
+			uniformModel="\tmtm\pack\altyn\tmtm_altyn_visorDown.p3d";
+			modelSides[]={6};
+			hiddenSelections[]={"camo"};
+			hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_green_co.paa"};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitpointName="HitHead";
+					armor=10;
+					passThrough=0.1;
+				};
+				class Face
+				{
+					hitpointName="HitFace";
+					armor=10;
+					passThrough=0.1;
+				};
+			};
+		};
+	};
+	class tmtm_h_altynVisorDown_olive: tmtm_h_altynVisorDown_green
+	{
+		scope=2;
+		displayName="Altyn (Olive/Visor Down)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_olive_co.paa"};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_olive_co.paa"};
+		};
+	};
+	class tmtm_h_altynVisorDown_black: tmtm_h_altynVisorDown_green
+	{
+		scope=2;
+		displayName="Altyn (Black/Visor Down)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_black_co.paa"};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\altyn\Data\tmtm_altyn_black_co.paa"};
+		};
+	};
 };
