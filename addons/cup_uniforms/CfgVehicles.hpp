819704460f7ac5d169fa706a19534c319f226269
trenchgun <sirpyrotiger@gmail.com>
2022-08-11 20:20:57 -0500

    added Nomex Flightsuit (Grey)

--- addons/cup_uniforms/CfgVehicles.hpp
+++ addons/cup_uniforms/CfgVehicles.hpp
@@ -490,4 +490,26 @@ class CfgVehicles
 			"\CUP\Creatures\People\Military\CUP_Creatures_People_Military_Russia\data\oakley_2_od_co.paa"
 		};
 	};
+
+	class CUP_B_USMC_Soldier_06;
+
+	class tmtm_nomex_grey: CUP_B_USMC_Soldier_06
+	{
+		author="trenchgun";
+
+		scope=1;
+		scopeArsenal=2;
+		scopeCurator=0;
+
+		displayName="Nomex Flightsuit (Grey)";
+		uniformClass="tmtm_u_nomex_grey";
+
+		picture="\CUP\Creatures\People\Military\CUP_Creatures_People_Military_USMC\data\ui\icon_u_b_usmc_pilotoverall_ca.paa";
+		hiddenSelections[] = {"Camo", "Camo2"};
+		hiddenSelectionsTextures[] = 
+		{
+			"\tmtm\pack\cup_uniforms\Data\tmtm_nomex_grey_co.paa",
+			"\cup\creatures\people\military\cup_creatures_people_military_usmc\data\gloves_co.paa"
+		};
+	};
 };
