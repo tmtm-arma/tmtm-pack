4bc083ee731e92eec26a1bd1bad33b99e051ef96
trenchgun <sirpyrotiger@gmail.com>
2022-09-25 16:40:42 -0500

    added UI icons to Camo Netting, removed facewear from randomization

---
+++ addons/blufor_facewear/README.md
@@ -0,0 +1,8 @@
+# BLUFOR Facewear
+BLUFOR faction facewear retextures.
+
+## Camo Netting
+	- Black
+	- Coyote
+	- Grey
+	- Olive
---
+++ addons/blufor_facewear/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT blufor_facewear
+#define COMPONENT_BEAUTIFIED BLUFOR Facewear
+#include "\tmtm\pack\main\script_mod.hpp"
+#include "\tmtm\pack\main\script_macros.hpp"
