4a1886b875e4dfa811850e51a39d9e1ec6b9e53b
erem2k <erem275@gmail.com>
2022-09-27 13:47:35 +0200

    Add M88 field cap

--- addons/cup_headgear/CfgWeapons.hpp
+++ addons/cup_headgear/CfgWeapons.hpp
@@ -341,6 +341,40 @@ class CfgWeapons
 		};
 	};
 
+	// Field Cap (M-88)
+	class tmtm_h_m88_fieldcap_base: ItemCore
+	{
+		scope=0;
+		scopeCurator=0;
+		scopeArsenal=0;
+
+		displayName="Field Cap (M88)";
+
+		model = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_Chedaki\CUP_Ins_OfficerCap.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {""};
+		class ItemInfo: HeadgearItem
+		{
+			mass = 10;
+			uniformModel="\CUP\Creatures\People\Military\CUP_Creatures_People_Military_Chedaki\CUP_Ins_OfficerCap.p3d";
+			allowedSlots[] = {UNIFORM_SLOT, BACKPACK_SLOT, VEST_SLOT, HEADGEAR_SLOT};
+			modelSides[] = {6};
+			armor = 0;
+			passThrough = 1;
+			hiddenSelections[] = {"camo"};
+			hiddenSelectionsTextures[] = {""};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitpointName="HitHead";
+					armor=0;
+					passThrough=1;
+				};
+			};
+		};
+	};
+
 	// Basic ACH helmet
 	class CUP_H_USArmy_HelmetACH_UCP: ItemCore {};
 
@@ -594,6 +628,25 @@ class CfgWeapons
 		};
 	};
 
+	// Field Cap (M-88/Khaki)
+	class tmtm_h_m88_fieldcap_khaki: tmtm_h_m88_fieldcap_base
+	{
+		author="erem2k";
+		
+		scope=2;
+		scopeCurator=2;
+		scopeArsenal=2;
+
+		displayName  = "Field Cap (M-88/Khaki)";
+		picture="\tmtm\pack\cup_headgear\Data\ui\icon_tmtm_h_cap_mm14.paa";
+
+		hiddenSelectionsTextures[] = {"\tmtm\pack\cup_headgear\Data\tmtm_m88_fieldcap_khaki_co.paa"};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[] = {"\tmtm\pack\cup_headgear\Data\tmtm_m88_fieldcap_khaki_co.paa"};
+		};
+	};
+	
 	// MICH 2000 (Black)
 	class tmtm_h_ACH_black: CUP_H_USArmy_HelmetACH_UCP
 	{
