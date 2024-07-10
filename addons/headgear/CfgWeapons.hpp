3ba065f5759221814ee8ed87040053e504d48fa9
erem2k <nope@nohost.com>
2024-07-10 02:30:41 +0200

    headgear - upload and config CPD K6-3 retextures

--- addons/headgear/CfgWeapons.hpp
+++ addons/headgear/CfgWeapons.hpp
@@ -527,7 +527,62 @@ class CfgWeapons
 			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_surpat_co.paa"};
 		};
 	};
-	
+	class tmtm_h_k6_cpd_ameba: tmtm_h_k6_teal
+	{
+		scope=2;
+		displayName="CPD K6-3 (Ameba)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_ameba_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_ameba_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_black: tmtm_h_k6_teal
+	{
+		scope=2;
+		displayName="CPD K6-3 (Black)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_black_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_black_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_dubokb: tmtm_h_k6_teal
+	{
+		scope=2;
+		displayName="CPD K6-3 (Dubok Blue)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokb_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokb_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_dubokgb: tmtm_h_k6_teal
+	{
+		scope=2;
+		displayName="CPD K6-3 (Dubok Gray)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokgb_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokgb_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_olive: tmtm_h_k6_teal
+	{
+		scope=2;
+		displayName="CPD K6-3 (Olive)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_olive_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_olive_co.paa"};
+		};
+	};
+
 	// K6-3 (Visor Up)
 	class tmtm_h_k6_teal_visorUp: ItemCore
 	{
@@ -711,7 +766,62 @@ class CfgWeapons
 			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_surpat_co.paa"};
 		};
 	};
-	
+	class tmtm_h_k6_cpd_ameba_visorUp: tmtm_h_k6_teal_visorUp
+	{
+		scope=2;
+		displayName="CPD K6-3 (Ameba/Visor Up)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_ameba_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorUp_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_ameba_visor_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_black_visorUp: tmtm_h_k6_teal_visorUp
+	{
+		scope=2;
+		displayName="CPD K6-3 (Black/Visor Up)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_black_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorUp_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_black_visor_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_dubokb_visorUp: tmtm_h_k6_teal_visorUp
+	{
+		scope=2;
+		displayName="CPD K6-3 (Dubok Blue/Visor Up)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokb_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorUp_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokb_visor_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_dubokgb_visorUp: tmtm_h_k6_teal_visorUp
+	{
+		scope=2;
+		displayName="CPD K6-3 (Dubok Gray/Visor Up)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokgb_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorUp_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokgb_visor_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_olive_visorUp: tmtm_h_k6_teal_visorUp
+	{
+		scope=2;
+		displayName="CPD K6-3 (Olive/Visor Up)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_olive_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorUp_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_olive_visor_co.paa"};
+		};
+	};
+
 	// K6-3 (Visor Down)
 	class tmtm_h_k6_teal_visorDown: ItemCore
 	{
@@ -899,6 +1009,61 @@ class CfgWeapons
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_surpat_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_ameba_visorDown: tmtm_h_k6_teal_visorDown
+	{
+		scope=2;
+		displayName="CPD K6-3 (Ameba/Visor Down)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_ameba_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorDown_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_ameba_visor_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_black_visorDown: tmtm_h_k6_teal_visorDown
+	{
+		scope=2;
+		displayName="CPD K6-3 (Black/Visor Down)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_black_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorDown_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_black_visor_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_dubokb_visorDown: tmtm_h_k6_teal_visorDown
+	{
+		scope=2;
+		displayName="CPD K6-3 (Dubok Blue/Visor Down)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokb_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorDown_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokb_visor_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_dubokgb_visorDown: tmtm_h_k6_teal_visorDown
+	{
+		scope=2;
+		displayName="CPD K6-3 (Dubok Gray/Visor Down)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokgb_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorDown_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_dubokgb_visor_co.paa"};
+		};
+	};
+	class tmtm_h_k6_cpd_olive_visorDown: tmtm_h_k6_teal_visorDown
+	{
+		scope=2;
+		displayName="CPD K6-3 (Olive/Visor Down)";
+		hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_olive_visor_co.paa"};
+		picture="\tmtm\pack\headgear\Data\UI\tmtm_icon_k6_teal_visorDown_ca.paa";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]={"\tmtm\pack\headgear\Data\tmtm_k6_cpd_olive_visor_co.paa"};
 		};
 	};
 };
