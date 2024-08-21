d5cbb47f42c1afedd68d8676d9274e4d77195fca
erem2k <nope@nohost.com>
2024-08-22 01:49:08 +0200

    faction_glf_cup - add olive M92 variants

---
+++ addons/faction_glf_cup/characters/headgear/CfgWeapons.hpp
@@ -0,0 +1,187 @@
+class tmtm_h_m92_base: ItemCore
+{
+    class ItemInfo;
+};
+
+class tmtm_h_m92_net_base: tmtm_h_m92_base
+{
+    class ItemInfo;
+};
+
+class tmtm_h_m92_goggles_base: tmtm_h_m92_base
+{
+    class ItemInfo;
+};
+
+class tmtm_h_m92_goggles_f_base: tmtm_h_m92_goggles_base
+{
+    class ItemInfo;
+};
+
+class tmtm_h_m92_goggles_b_base: tmtm_h_m92_goggles_base
+{
+    class ItemInfo;
+};
+
+class tmtm_h_m92_covered_base: tmtm_h_m92_base
+{
+    class ItemInfo;
+};
+
+class tmtm_h_m92_covered_goggles_base: tmtm_h_m92_covered_base
+{
+    class ItemInfo;
+};
+
+class tmtm_h_m92_covered_goggles_f_base: tmtm_h_m92_covered_goggles_base
+{
+    class ItemInfo;
+};
+
+class tmtm_h_m92_covered_goggles_b_base: tmtm_h_m92_covered_goggles_base
+{
+    class ItemInfo;
+};
+
+// Olive
+class tmtm_h_m92_glf_olive: tmtm_h_m92_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="GLF M92 Helmet (Olive)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_co.paa"
+        };
+    };
+};
+
+class tmtm_h_m92_glf_olive_crest: tmtm_h_m92_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="GLF M92 Helmet (Olive, Crest)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_crest_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_crest_co.paa"
+        };
+    };
+};
+
+class tmtm_h_m92_net_glf_olive: tmtm_h_m92_net_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="GLF M92 Helmet (Olive, Net)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_net_glf_olive_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_net_glf_olive_co.paa"
+        };
+    };
+};
+
+class tmtm_h_m92_goggles_glf_olive: tmtm_h_m92_goggles_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="GLF M92 Helmet (Olive, Goggles)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_co.paa"
+        };
+    };
+};
+
+class tmtm_h_m92_goggles_f_glf_olive: tmtm_h_m92_goggles_f_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="GLF M92 Helmet (Olive, Goggles)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_co.paa"
+        };
+    };
+};
+
+class tmtm_h_m92_goggles_b_glf_olive: tmtm_h_m92_goggles_b_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="GLF M92 Helmet (Olive, Goggles)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_glf_cup\characters\headgear\Data\tmtm_m92_glf_olive_co.paa"
+        };
+    };
+};
+
+// Cover (Brush Woodland)
+
+// Cover (Brush Urban)
