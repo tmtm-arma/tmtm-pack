f5eb5ebac70cc270b35659bcd5dddcb40fa4f43c
erem2k <nope@nohost.com>
2024-08-25 22:07:15 +0200

    faction_cdf - upload dummy addon

---
+++ addons/faction_cdf/README.md
@@ -0,0 +1,5 @@
+# CDF: General Retextures
+Retextures of vanilla and TMTM pack gear and vehicles for CDF.
+
+## Headgear
+### K6-3 Helmet
---
+++ addons/faction_cdf/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT faction_cdf
+#define COMPONENT_BEAUTIFIED CDF Faction (Generic Content)
+#include "\tmtm\pack\main\script_mod.hpp"
+#include "\tmtm\pack\main\script_macros.hpp"
