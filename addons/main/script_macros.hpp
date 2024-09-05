c115ce241a078427685c8bd1567f7bfce83a467d
erem2k <nope@nohost.com>
2024-09-05 21:54:13 +0200

    main - add macro for no armor uniform hitpoints

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -55,3 +55,141 @@
 	tf_hasLRradio = 1;\
 	tf_range = 20000;\
 	tf_subtype = "digital_lr";
+
+#define HITPOINTS_UNIFORM_NOARMOR\
+	class HitFace\
+	{\
+		armor=1;\
+		material=-1;\
+		name="face_hub";\
+		passThrough=0.80000001;\
+		radius=0.079999998;\
+		explosionShielding=0.1;\
+		minimalHit=0.0099999998;\
+	};\
+	class HitNeck: HitFace\
+	{\
+		armor=1;\
+		material=-1;\
+		name="neck";\
+		passThrough=0.80000001;\
+		radius=0.1;\
+		explosionShielding=0.5;\
+		minimalHit=0.0099999998;\
+	};\
+	class HitHead: HitNeck\
+	{\
+		armor=1;\
+		material=-1;\
+		name="head";\
+		passThrough=0.80000001;\
+		radius=0.2;\
+		explosionShielding=0.5;\
+		minimalHit=0.0099999998;\
+		depends="HitFace max HitNeck";\
+	};\
+	class HitPelvis: HitHead\
+	{\
+		armor=6;\
+		material=-1;\
+		name="pelvis";\
+		passThrough=0.80000001;\
+		radius=0.23999999;\
+		explosionShielding=1;\
+		visual="injury_body";\
+		minimalHit=0.0099999998;\
+		depends="0";\
+	};\
+	class HitAbdomen: HitPelvis\
+	{\
+		armor=1;\
+		material=-1;\
+		name="spine1";\
+		passThrough=0.80000001;\
+		radius=0.16;\
+		explosionShielding=1;\
+		visual="injury_body";\
+		minimalHit=0.0099999998;\
+	};\
+	class HitDiaphragm: HitAbdomen\
+	{\
+		armor=1;\
+		material=-1;\
+		name="spine2";\
+		passThrough=0.80000001;\
+		radius=0.18000001;\
+		explosionShielding=6;\
+		visual="injury_body";\
+		minimalHit=0.0099999998;\
+	};\
+	class HitChest: HitDiaphragm\
+	{\
+		armor=1;\
+		material=-1;\
+		name="spine3";\
+		passThrough=0.80000001;\
+		radius=0.18000001;\
+		explosionShielding=6;\
+		visual="injury_body";\
+		minimalHit=0.0099999998;\
+	};\
+	class HitBody: HitChest\
+	{\
+		armor=1000;\
+		material=-1;\
+		name="body";\
+		passThrough=1;\
+		radius=0;\
+		explosionShielding=6;\
+		visual="injury_body";\
+		minimalHit=0.0099999998;\
+		depends="HitPelvis max HitAbdomen max HitDiaphragm max HitChest";\
+	};\
+	class HitArms: HitBody\
+	{\
+		armor=3;\
+		material=-1;\
+		name="arms";\
+		passThrough=1;\
+		radius=0.1;\
+		explosionShielding=1;\
+		visual="injury_hands";\
+		minimalHit=0.0099999998;\
+		depends="0";\
+	};\
+	class HitHands: HitArms\
+	{\
+		armor=3;\
+		material=-1;\
+		name="hands";\
+		passThrough=1;\
+		radius=0.1;\
+		explosionShielding=1;\
+		visual="injury_hands";\
+		minimalHit=0.0099999998;\
+		depends="HitArms";\
+	};\
+	class HitLegs: HitHands\
+	{\
+		armor=3;\
+		material=-1;\
+		name="legs";\
+		passThrough=1;\
+		radius=0.14;\
+		explosionShielding=1;\
+		visual="injury_legs";\
+		minimalHit=0.0099999998;\
+		depends="0";\
+	};\
+	class Incapacitated: HitLegs\
+	{\
+		armor=1000;\
+		material=-1;\
+		name="body";\
+		passThrough=1;\
+		radius=0;\
+		explosionShielding=1;\
+		visual="";\
+		minimalHit=0;\
+		depends="(((Total - 0.25) max 0) + ((HitHead - 0.25) max 0) + ((HitBody - 0.25) max 0)) * 2";\
+	};\
