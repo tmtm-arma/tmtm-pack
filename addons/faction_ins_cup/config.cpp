490c3c84f40f53592bd9e745731565be2863f503
erem2k <nope@nohost.com>
2024-09-15 00:26:08 +0200

    faction_ins_cup - upload dummy addon

---
+++ addons/faction_ins_cup/config.cpp
@@ -0,0 +1,22 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+	class ADDON {
+		name = COMPONENT_NAME;
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {
+			"tmtmpack_main",
+			"tmtmpack_cup_headgear"
+		};
+		author = MOD_AUTHOR;
+		url = MOD_URL;
+		version = VERSION;
+		versionStr = QUOTE(VERSION_STR);
+		versionAr[] = {QUOTE(VERSION_AR)};
+		skipWhenMissingDependencies=1;
+	};
+};
+
+#include "CfgWeapons.hpp"
---
+++ addons/faction_ins_cup/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT faction_ins_cup
+#define COMPONENT_BEAUTIFIED Insurgents Faction (CUP Content)
+#include "\tmtm\pack\main\script_mod.hpp"
+#include "\tmtm\pack\main\script_macros.hpp"
