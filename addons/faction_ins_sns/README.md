450567858f665f57be15d1f7ef3e3e8f63e2168b
erem2k <nope@nohost.com>
2024-09-15 20:46:12 +0200

    faction_ins_sns - upload addon, add hwcaps

---
+++ addons/faction_ins_sns/README.md
@@ -0,0 +1,10 @@
+# Insurgents: S&S retextures
+Retextures of S&S gear for generic insurgents.
+
+## Headgear
+### Field Cap
+- Birch
+- Fir Green
+- Fir Tan
+- Barvikha
+- Needles
---
+++ addons/faction_ins_sns/characters/headgear/CfgWeapons.hpp
@@ -0,0 +1,239 @@
+class tmtm_h_hwcap_base: HelmetBase
+{
+    class ItemInfo;
+};
+
+class tmtm_h_hwcap_alt_base: tmtm_h_hwcap_base
+{
+    class ItemInfo;
+};
+
+class tmtm_h_hwcap_ins_birch: tmtm_h_hwcap_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Birch)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_birch_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_birch_co.paa"
+        };
+    };
+};
+
+class tmtm_h_hwcap_ins_birch_alt: tmtm_h_hwcap_alt_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Birch)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_birch_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_birch_co.paa"
+        };
+    };
+};
+
+class tmtm_h_hwcap_ins_butan: tmtm_h_hwcap_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Butan)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_butan_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_butan_co.paa"
+        };
+    };
+};
+
+class tmtm_h_hwcap_ins_butan_alt: tmtm_h_hwcap_alt_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Butan)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_butan_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_butan_co.paa"
+        };
+    };
+};
+
+class tmtm_h_hwcap_ins_butang: tmtm_h_hwcap_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Butan Green)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_butan_green_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_butan_green_co.paa"
+        };
+    };
+};
+
+class tmtm_h_hwcap_ins_butang_alt: tmtm_h_hwcap_alt_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Butan Green)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_butan_green_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_butan_green_co.paa"
+        };
+    };
+};
+
+class tmtm_h_hwcap_ins_firg: tmtm_h_hwcap_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Fir Green)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_fir_green_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_fir_green_co.paa"
+        };
+    };
+};
+
+class tmtm_h_hwcap_ins_firg_alt: tmtm_h_hwcap_alt_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Fir Green)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_fir_green_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_fir_green_co.paa"
+        };
+    };
+};
+
+class tmtm_h_hwcap_ins_firt: tmtm_h_hwcap_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Fir Tan)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_fir_tan_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_fir_tan_co.paa"
+        };
+    };
+};
+
+class tmtm_h_hwcap_ins_firt_alt: tmtm_h_hwcap_alt_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="Guerilla Field Cap (Fir Tan)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[]= {
+        "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_fir_tan_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[]= {
+            "\tmtm\pack\faction_ins_sns\characters\headgear\data\tmtm_hwcap_ins_fir_tan_co.paa"
+        };
+    };
+};
