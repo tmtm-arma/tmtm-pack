80bfb636e05efbbb94239eab19bccd392f68803a
erem2k <nope@nohost.com>
2024-09-16 03:17:41 +0200

    faction_ins_cup - add insurgent ssh-68s

---
+++ addons/faction_ins_cup/characters/headgear/CfgWeapons.hpp
@@ -0,0 +1,101 @@
+class tmtm_h_ssh68_base;
+class tmtm_h_ssh68_covered_base: tmtm_h_ssh68_base
+{
+    class ItemInfo;
+};
+
+class tmtm_h_ssh68_covered_ins_makaronid: tmtm_h_ssh68_covered_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="SSh-68, Covered (Needles)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_makaronid_co.paa",
+        "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_makaronid_co.paa"
+    };
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[] = {
+            "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_makaronid_co.paa",
+            "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_makaronid_co.paa"
+        };
+    };
+};
+
+class tmtm_h_ssh68_covered_ins_butan: tmtm_h_ssh68_covered_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="SSh-68, Covered (Butan)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_butan_co.paa",
+        "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_butan_co.paa"
+    };
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[] = {
+            "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_butan_co.paa",
+            "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_butan_co.paa"
+        };
+    };
+};
+
+class tmtm_h_ssh68_covered_ins_firg: tmtm_h_ssh68_covered_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="SSh-68, Covered (Fir Green)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_fir_green_co.paa",
+        "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_fir_green_co.paa"
+    };
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[] = {
+            "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_fir_green_co.paa",
+            "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_fir_green_co.paa"
+        };
+    };
+};
+
+class tmtm_h_ssh68_covered_ins_barvikha: tmtm_h_ssh68_covered_base
+{
+    author="erem2k";
+
+    scope=2;
+    scopeCurator=2;
+    scopeArsenal=2;
+
+    displayName="SSh-68, Covered (Barvikha)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_barvikha_co.paa",
+        "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_barvikha_co.paa"
+    };
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[] = {
+            "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_barvikha_co.paa",
+            "\tmtm\pack\faction_ins_cup\characters\headgear\data\tmtm_ssh68_covered_barvikha_co.paa"
+        };
+    };
+};
