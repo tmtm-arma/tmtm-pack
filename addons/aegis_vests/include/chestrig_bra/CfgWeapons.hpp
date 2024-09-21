aa9776612b73a04e387cbaec71ca093e239b02e0
erem2k <nope@nohost.com>
2024-09-21 23:59:17 +0200

    aegis_vests - add brown bra chest rig, texture pass on the rest

--- addons/aegis_vests/include/chestrig_bra/CfgWeapons.hpp
+++ addons/aegis_vests/include/chestrig_bra/CfgWeapons.hpp
@@ -4,75 +4,183 @@ class tmtm_v_chestrig_bra_base: Vest_Camo_Base
     scopeArsenal=0;
 
     model="\tmtm\pack\aegis_vests\tmtm_v_chestrig_bra.p3d";
-    hiddenSelections[]= { "camo" };
+    hiddenSelections[]= { "camo", "camo_pouches" };
 
     class ItemInfo: ItemInfo
     {
         uniformModel="\tmtm\pack\aegis_vests\tmtm_v_chestrig_bra.p3d";
-        hiddenSelections[]= { "camo" };
+        hiddenSelections[]= { "camo", "camo_pouches" };
 
         containerClass="Supply140";
         mass=10;
     };
 };
 
-class tmtm_v_chestrig_bra_old_base: tmtm_v_chestrig_bra_base
+class tmtm_v_chestrig_bra_vintage_base: tmtm_v_chestrig_bra_base
 {
     scope=1;
     scopeArsenal=0;
 
-    hiddenSelectionsMaterials[] = {
-        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_old.rvmat"
+    hiddenSelectionsMaterials[]= {
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_vintage.rvmat",
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_vintage.rvmat"
     };
 
     class ItemInfo: ItemInfo
     {
-        hiddenSelectionsMaterials[] = {
-            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_old.rvmat"
+        hiddenSelectionsMaterials[]= {
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_vintage.rvmat",
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_vintage.rvmat"
         };
     };
 };
 
 /////////////////////////////////////////////
 // Content
-class tmtm_v_chestrig_bra_khaki: tmtm_v_chestrig_bra_base
+class tmtm_v_chestrig_bra_khaki: tmtm_v_chestrig_bra_vintage_base
 {
     author="OokamiJamie";
     scope=2;
     scopeArsenal=2;
 
-    displayName="Bra Chestrig, Khaki";
+    displayName="Bra Chest Rig, Khaki";
     picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
 
     hiddenSelectionsTextures[] = {
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_khaki_co.paa",
         "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_khaki_co.paa"
     };
 
     class ItemInfo: ItemInfo
     {
         hiddenSelectionsTextures[] = {
-            "\tmtm\pack\sns_vests\data\tmtm_chestrig_bra_khaki_co.paa"
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_khaki_co.paa",
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_khaki_co.paa"
         };
     };
 };
 
-class tmtm_v_chestrig_bra_green: tmtm_v_chestrig_bra_base
+class tmtm_v_chestrig_bra_khaki_flak: tmtm_v_chestrig_bra_vintage_base
 {
-    author="OokamiJamie";
+    author="OokamiJamie, erem2k";
     scope=2;
-    scopeArsenal=2;
+    scopeArsenal=0;
 
-    displayName="Bra Chestrig, Green";
+    displayName="Bra Chest Rig, Khaki";
     picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
 
     hiddenSelectionsTextures[] = {
+        "",
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_khaki_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[] = {
+            "",
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_khaki_co.paa"
+        };
+    };
+};
+
+class tmtm_v_chestrig_bra_brown: tmtm_v_chestrig_bra_vintage_base
+{
+    author="OokamiJamie, erem2k";
+    scope=2;
+    scopeArsenal=2;
+
+    displayName="Bra Chest Rig, Brown";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_brown_co.paa",
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_brown_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[] = {
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_brown_co.paa",
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_brown_co.paa"
+        };
+    };
+};
+
+class tmtm_v_chestrig_bra_brown_flak: tmtm_v_chestrig_bra_vintage_base
+{
+    author="OokamiJamie, erem2k";
+    scope=2;
+    scopeArsenal=0;
+
+    displayName="Bra Chest Rig, Brown (Flak)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "",
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_brown_co.paa"
+    };
+
+    hiddenSelectionsMaterials[]= {
+        "",
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_vintage.rvmat"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[] = {
+            "",
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_brown_co.paa"
+        };
+
+        hiddenSelectionsMaterials[]= {
+            "",
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_vintage.rvmat"
+        };
+    };
+};
+
+class tmtm_v_chestrig_bra_green: tmtm_v_chestrig_bra_vintage_base
+{
+    author="OokamiJamie, erem2k";
+    scope=2;
+    scopeArsenal=2;
+
+    displayName="Bra Chest Rig, Green";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_green_co.paa",
         "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_green_co.paa"
     };
 
     class ItemInfo: ItemInfo
     {
         hiddenSelectionsTextures[] = {
-            "\tmtm\pack\sns_vests\data\tmtm_chestrig_bra_green_co.paa"
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_green_co.paa",
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_green_co.paa"
+        };
+    };
+};
+
+class tmtm_v_chestrig_bra_green_flak: tmtm_v_chestrig_bra_vintage_base
+{
+    author="OokamiJamie, erem2k";
+    scope=2;
+    scopeArsenal=0;
+
+    displayName="Bra Chest Rig, Green (Flak)";
+    picture="\tmtm\pack\main\data\ui\icon_tmtm_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "",
+        "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_green_co.paa"
+    };
+
+    class ItemInfo: ItemInfo
+    {
+        hiddenSelectionsTextures[] = {
+            "",
+            "\tmtm\pack\aegis_vests\data\tmtm_chestrig_bra_green_co.paa"
         };
     };
 };
