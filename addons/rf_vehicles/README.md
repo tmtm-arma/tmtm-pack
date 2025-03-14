d0d110afc724f8fff6483192753b92cdd36fac34
erem2k <nope@nohost.com>
2025-03-14 10:15:26 +0100

    rf_vehicles - add winterized MOOSE pickup variants

---
+++ addons/rf_vehicles/README.md
@@ -0,0 +1,2 @@
+# RF Vehicles
+Adds new camos for Reaction Forces CDLC vehicles.
---
+++ addons/rf_vehicles/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT rf_vehicles
+#define COMPONENT_BEAUTIFIED RF Vehicles
+#include "\tmtm\pack\main\script_mod.hpp"
+#include "\tmtm\pack\main\script_macros.hpp"
